a2923d9d5159f5e008096cca2eb9707051005803
jacksonsix <jacksonsixmailbox@gmail.com>
2017-12-25 23:04:57 -0500

    Update memory.scm

--- memory.scm
+++ memory.scm
@@ -1,6 +1,14 @@
+(define reg1 'unassigned)
+(define reg2 'unassigned)
+
+(define the-cars (list 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0))
+(define the-cdrs (list 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0))
+
 (define (list-set! set off val)
   (if (= off 0)
       (set-car! set val)
       (list-set! (cdr set) (- off 1) val)))
       
       
+(define (assign target op oprand)
+   ((cdr op) orand))
