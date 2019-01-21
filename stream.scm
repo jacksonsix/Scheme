a384ca66a26c64d907a80e786e1e14387a908f89
jacksonsix <jacksonsixmailbox@gmail.com>
2019-01-21 15:29:16 -0500

    Update stream.scm

--- stream.scm
+++ stream.scm
@@ -220,6 +220,13 @@
             (add-streams (scale-stream (stream-cdr s2) (stream-car s1))
                           (mul-series  s2 (stream-cdr s1)))))
   
+;; another version  
+(define (mul-series s1 s2)
+  (cons-stream
+    (* (stream-car s1) (stream-car s2))
+    (add-streams (add-streams (scale-stream (stream-cdr s1) (stream-car s2))
+                              (scale-stream (stream-cdr s2) (stream-car s1)))
+                 (cons-stream 0 (mul-series (stream-cdr s1) (stream-cdr s2))))))  
   
 ;; define series as a stream a0 , a1,a2....
 (define (integrate-series stream)
